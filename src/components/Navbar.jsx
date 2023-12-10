4a19dfecd8dbb71e887361e580e44e208c28931d
TanutsaponJ <anakin112333@gmail.com>
2023-12-10 11:09:18 -0500

    Update Navbar component with new logo and button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,27 +2,48 @@ import React from "react";
 
 // ! import all file
 
-import Logo from "../assets/logo3.svg";
+import Logo from "../assets/logo.svg";
+import Button from "../layout/Button";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row justify-between items-center md:px-32 p-4 m-2">
-      <div>
-        <img src={Logo} alt="logoImage" />
-      </div>
+    <div className="flex flex-row justify-between items-center md:px-32 px-5 p-4 m-2">
+      <div className="flex flex-row gap-12 items-center text-lightText p-2">
+        <div>
+          <img src={Logo} alt="logoImage" />
+        </div>
 
-      <nav className="flex gap-4">
-        <a href="/work">How it work</a>
-        <a href="/gallery">Design Gallery</a>
-        <a href="/architects">Architects</a>
-        <a href="/articles">Articles</a>
-      </nav>
+        <nav className="hidden md:flex gap-5 ">
+          <a
+            className="hover:text-brightRed transition-all ease-in-out"
+            href="/work"
+          >
+            How it work
+          </a>
+          <a
+            className="hover:text-brightRed transition-all ease-in-out"
+            href="/gallery"
+          >
+            Design Gallery
+          </a>
+          <a
+            className="hover:text-brightRed transition-all ease-in-out"
+            href="/architects"
+          >
+            Architects
+          </a>
+          <a
+            className="hover:text-brightRed transition-all ease-in-out"
+            href="/articles"
+          >
+            Articles
+          </a>
+        </nav>
+      </div>
 
       <div className="flex flex-row items-center p-2 gap-4">
         <a href="/signIn">Sign In</a>
-        <button className="bg-brightRed px-4 py-2 rounded-md hover:bg-white hover:text-black hover:scale-105 transition-all">
-          Sign Up
-        </button>
+        <Button title="Sign Up" />
       </div>
     </div>
   );
---
+++ src/layout/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <button className="bg-brightRed px-4 py-2 rounded-md hover:bg-white hover:text-black hover:scale-105 transition-all">
+      {props.title}
+    </button>
+  );
+};
+
+export default Button;
